613424a6824bf567c9799697588ee06f585dcf44
neung pornpavee <pornpavee.s@fairplaystudios.net>
2025-08-08 21:53:35 +0700

    simple grass anim

--- Assets/Scripts/Grass.cs
+++ Assets/Scripts/Grass.cs
@@ -5,6 +5,9 @@ public class Grass : MonoBehaviour
     [SerializeField]
     private float _regrowDuration = 3f;
 
+    [SerializeField]
+    private Animator _animator;
+
     public bool IsAvailable { get; private set; } = true;
 
     public void GetEaten()
@@ -15,14 +18,13 @@ public class Grass : MonoBehaviour
         }
 
         IsAvailable = false;
-        gameObject.SetActive(false);
-
+        _animator.SetTrigger("Disappear");
         Invoke(nameof(Regrow), _regrowDuration);
     }
 
     private void Regrow()
     {
+        _animator.SetTrigger("Appear");
         IsAvailable = true;
-        gameObject.SetActive(true);
     }
 }
