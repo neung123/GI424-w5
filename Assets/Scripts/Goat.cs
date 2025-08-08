5c7c5b29cd53ca0d2e5a411eec88d84eaf1bcfc1
neung pornpavee <pornpavee.s@fairplaystudios.net>
2025-08-08 21:39:39 +0700

    goat eating anim

--- Assets/Scripts/Goat.cs
+++ Assets/Scripts/Goat.cs
@@ -1,12 +1,18 @@
-using UnityEngine;
 using System.Collections;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class Goat : MonoBehaviour
 {
     [SerializeField]
     private float _eatDuration = 1f;
+
+    [SerializeField]
+    private float _moveSpeed = 2f;
+
+    [SerializeField]
+    private Animator _animator;
 
     private List<Grass> _allGrass;
 
@@ -26,14 +32,48 @@ public class Goat : MonoBehaviour
             {
                 Grass target = availableGrass[Random.Range(0, availableGrass.Count)];
 
-                transform.position = target.transform.position;
+                yield return StartCoroutine(MoveToTarget(target.transform.position));
+
+
+                _animator.SetBool("Eat", true);
 
                 yield return new WaitForSeconds(_eatDuration);
+
+                _animator.SetBool("Eat", false);
 
                 target.GetEaten();
             }
 
             yield return null;
         }
+    }
+
+    private IEnumerator MoveToTarget(Vector3 targetPos)
+    {
+        Vector3 startPos = transform.position;
+        float distance = Vector3.Distance(startPos, targetPos);
+        float duration = distance / _moveSpeed;
+
+        _animator.SetBool("Walking", true);
+
+        Vector3 direction = (targetPos - startPos).normalized;
+
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        _animator.SetBool("Walking", false);
     }
 }
