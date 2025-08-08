a3880c158a64e8161b85cd81da94ae5d9a98d6f0
neung pornpavee <pornpavee.s@fairplaystudios.net>
2025-08-08 22:11:13 +0700

    changed logic, can pick to generate or not

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
+    private bool _isGenerateGrass;
+
+    [SerializeField]
+    private List<Grass> _generateGrass;
+
+    [SerializeField]
     private Transform _grassHolder;
 
     [SerializeField]
@@ -34,18 +40,30 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        for (int r = 0; r < _grassRows; r++)
+        if (_isGenerateGrass)
         {
-            for (int c = 0; c < _grassCols; c++)
+            foreach (var g in _generateGrass)
             {
-                // pick a random grass prefab from the list
-                Grass prefab = _grassPrefabs[Random.Range(0, _grassPrefabs.Count)];
-
-                Vector3 pos = new Vector3(c * _grassSpacing, 0, r * _grassSpacing);
-                Grass grass = Instantiate(prefab, pos, Quaternion.identity);
-                grass.transform.parent = _grassHolder;
-                _allGrass.Add(grass);
+                g.gameObject.SetActive(false);
             }
+
+            for (int r = 0; r < _grassRows; r++)
+            {
+                for (int c = 0; c < _grassCols; c++)
+                {
+                    // pick a random grass prefab from the list
+                    Grass prefab = _grassPrefabs[Random.Range(0, _grassPrefabs.Count)];
+
+                    Vector3 pos = new Vector3(c * _grassSpacing, 0, r * _grassSpacing);
+                    Grass grass = Instantiate(prefab, pos, Quaternion.identity);
+                    grass.transform.parent = _grassHolder;
+                    _allGrass.Add(grass);
+                }
+            }
+        }
+        else
+        {
+            _allGrass.AddRange(_generateGrass);
         }
 
         Instantiate(_goatPrefab, new Vector3(0, 0, 0), Quaternion.identity);
